211e7748e95e628e6a514a795687f5ae1defb2da
Trystan Merrien <merrien2u@iutsd-127-43.local>
2020-01-27 08:35:54 +0100

    Fin de la partie 2 Tuto

--- MoonRunner/Model/Badges/BadgeCell.swift
+++ MoonRunner/Model/Badges/BadgeCell.swift
@@ -19,8 +19,8 @@ class BadgeCell: UITableViewCell {
     }
   }
   
-  private let redLabel = #colorLiteral(red: 1, green: 0.07843137255, blue: 0.1725490196, alpha: 1)
-  private let greenLabel = #colorLiteral(red: 0, green: 0.5725490196, blue: 0.3058823529, alpha: 1)
+  private let redLabel = #colorLiteral(red: 0.7773455977, green: 0.1015973464, blue: 0.2125645578, alpha: 1)
+  private let greenLabel = #colorLiteral(red: 1, green: 0.8446780443, blue: 0.2366746068, alpha: 1)
   private let badgeRotation = CGAffineTransform(rotationAngle: .pi / 8)
   
   // Configuration l'ffaichage des cellulles dans la vue
