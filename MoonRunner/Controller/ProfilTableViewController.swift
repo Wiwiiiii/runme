d4bbe3c6e6da41df6ebdf6e5267b59149a5eea0b
Trystan Merrien <xwifou@gmail.com>
2020-01-29 13:34:27 +0100

    Commit master

--- MoonRunner/Controller/ProfilTableViewController.swift
+++ MoonRunner/Controller/ProfilTableViewController.swift
@@ -16,17 +16,11 @@ class ProfilTableViewController: UITableViewController {
   
   private func loadSampleProfils() {
     
-    guard let profil1 = Profil(sexe: "Homme", taille: 1.20, nom: "Caprese Salad", age: 15, prenom: "Tanguy", email: "wiwi@gmail.com", poids: 4.5) else {
-        fatalError("Unable to instantiate profil1")
-    }
+    let profil1 = Profil(sexe: "Homme", taille: 1.20, nom: "Caprese Salad", age: 15, prenom: "Tanguy", email: "wiwi@gmail.com", poids: 4.5)
     
-    guard let profil2 = Profil(sexe: "Homme", taille: 1.20, nom: "Caprese Salad", age: 15, prenom: "Tanguy", email: "wiwi@gmail.com", poids: 4.5) else {
-        fatalError("Unable to instantiate profil2")
-    }
+    let profil2 = Profil(sexe: "Homme", taille: 1.20, nom: "Caprese Salad", age: 15, prenom: "Tanguy", email: "wiwi@gmail.com", poids: 4.5)
     
-    guard let profil3 = Profil(sexe: "Homme", taille: 1.20, nom: "Caprese Salad", age: 15, prenom: "Tanguy", email: "wiwi@gmail.com", poids: 4.5) else {
-        fatalError("Unable to instantiate profil3")
-    }
+    let profil3 = Profil(sexe: "Homme", taille: 1.20, nom: "Caprese Salad", age: 15, prenom: "Tanguy", email: "wiwi@gmail.com", poids: 4.5)
     
     profils += [profil1, profil2, profil3]
       
@@ -46,19 +40,12 @@ class ProfilTableViewController: UITableViewController {
           fatalError("La cellule n'est pas une instance de ProfilCell.")
       }
       
-      // Fetches the appropriate meal for the data source layout.
+
       let profil = profils[indexPath.row]
-      
       cell.sexeLabelCellProfil.text = profil.sexe
-    cell.tailleLabelCellProfil.text?.doubleValueOrNil = profil.taille
       cell.prenomLabelCellProfil.text = profil.prenom
-    cell.poidsLabelCellProfil.text?.intValueOrNil = profil.poids
-      //cell.imageLabelCellProfil.text = profil.image
       cell.nomLabelCellProfil.text = profil.nom
-      cell.ageLabelCellProfil.text?.doubleValueOrNil = profil.age
-      
-      return cell
-      
+
       return cell
   }
 }
--- MoonRunner/Model/Profil/ProfilCell.swift
+++ MoonRunner/Model/Profil/ProfilCell.swift
@@ -4,12 +4,9 @@ import UIKit
 class ProfilCell: UITableViewCell{
   
   @IBOutlet weak var sexeLabelCellProfil: UILabel!
-  @IBOutlet weak var tailleLabelCellProfil: UILabel!
   @IBOutlet weak var prenomLabelCellProfil: UILabel!
-  @IBOutlet weak var poidsLabelCellProfil: UILabel!
   @IBOutlet weak var imageCellProfil: UIImageView!
   @IBOutlet weak var nomLabelCellProfil: UILabel!
-  @IBOutlet weak var ageLabelCellProfil: UILabel!
   
   
 }
